5f63ac26d60922dca1ab883b6e7ead67c4df7f86
David Sletten <david.sletten@target.com>
2021-03-22 15:19:43 -0500

    Router cleanup

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,10 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import App from "../components/app";
 
 function AppRouter() {
   return (
     <Router>
-      <Switch>
-        <Route path="/:route?/:direction?/:stop?" children={<App />} />
-      </Switch>
+      <Route path="/:route?/:direction?/:stop?" children={<App />} />
     </Router>
   );
 }
